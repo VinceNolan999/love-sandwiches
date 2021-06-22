68d4da4b82088e5d484f884274666c2ca0303459
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-06-22 15:35:24 +0000

    salaes data for last 5 days

--- run.py
+++ run.py
@@ -90,7 +90,6 @@ def update_worksheet(data, worksheet):
     print(f"{worksheet} worksheet updated successfully\n")
 
 
-
 def calculate_surplus_data(sales_row):
     """
     Compare sales with stock and calculate the surplus for each item type.
@@ -109,6 +108,19 @@ def calculate_surplus_data(sales_row):
     return surplus_data
 
 
+def get_last_5_entries_sales():
+    """
+    Collects columns of data from sales worksheet
+    """
+    sales = SHEET.worksheet("sales")
+    columns = []
+    for ind in range(1, 7):
+        column = sales.col_values(ind)
+        columns.append(column[-5:])
+    
+    return columns
+
+
 def main():
     """
     Run all program functions
@@ -122,4 +134,7 @@ def main():
     update_worksheet(new_surplus_data, "surplus")
 
 print("\nWelcome to Love Sandwiches Data Automation")
-main()
+#main()
+
+sales_columns = get_last_5_entries_sales()
+print(sales_columns)
