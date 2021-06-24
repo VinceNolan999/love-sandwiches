1d0e58df3e5bff6a665cfa2dd735c746b80a2f58
VinceNolan999 <vincenolan999@yahoo.co.uk>
2021-06-24 16:45:13 +0000

    finished love sandwiches

--- run.py
+++ run.py
@@ -151,13 +151,27 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("\nWelcome to Love Sandwiches Data Automation")
-#main()
+stock_data = main()
 
 
 def get_stock_values(data):
+    """
+    Print out the calculated stock numbers for each sandwich type.
+    """
     headings = SHEET.worksheet('stock').row_values(1)
-    return headings
+
+    print("Make the following numbers of sandwiches for next market:\n")
+
+    # new_data = {}
+    # for heading, stock_num in zip(headings, data):
+    #     new_data[heading] = stock_num
+    # return new_data
     
+    return {heading: data for heading, data in zip(headings, data)}
+    
+stock_values = get_stock_values(stock_data)
+print(stock_values)
